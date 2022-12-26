8809efbfa5283b9aa5477699984f1cf6701f2883
reboot297 <reboot297@gmail.com>
2022-12-26 19:15:24 +0200

    Support ktlint

--- app/src/main/java/com/reboot297/fcmtestingtool/MainActivity.kt
+++ app/src/main/java/com/reboot297/fcmtestingtool/MainActivity.kt
@@ -18,14 +18,14 @@ package com.reboot297.fcmtestingtool
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.navigation.NavigationView
 import com.reboot297.fcmtestingtool.databinding.ActivityMainBinding
 
 /**
@@ -51,7 +51,8 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home
-            ), drawerLayout
+            ),
+            drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
